e95043c57cbda3f909de25b4bd08f4781e50a742
Alimbetov Ruslan <kocapb@gmail.com>
2020-04-20 06:36:01 +0300

    added fix for stable-5.8

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,7 +48,7 @@ class Handler extends ExceptionHandler
      * @param Exception $exception
      * @return Response
      */
-    public function render($request, \Exception $exception)
+    public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
     }
